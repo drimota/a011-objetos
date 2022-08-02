f550e4909f8961428f3dd46f4c68b41ab4f7f41c
drimota <driismota@hotmail.com>
2022-08-01 20:33:36 -0400

    atividade 1 finalizada

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+const sacolao = []
+
+const feira = {
+    nome: "maça",
+    preço: 1,
+    disponibilidade: true,
+
+    nome: "banana",
+    preço: 5,
+    disponibilidade: false,
+
+    nome: "acerola",
+    preço: 6,
+    disponibilidade: true,
+}
+
+sacolao.push(feira)
+
+console.log(sacolao)
